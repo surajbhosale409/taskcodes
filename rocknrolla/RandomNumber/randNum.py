cc0a2e1bf0b650a8301cc82947fe0e204ac422b5
rocknrolla <rocknrolla@DESKTOP-IQH3KMS>
2017-06-26 11:47:47 -0400

    suraj's fourth commit

--- rocknrolla/RandomNumber/randNum.py
+++ rocknrolla/RandomNumber/randNum.py
@@ -1,48 +1,70 @@
 import os,random
 
+
+def initHints(randNum):
+  iniHints=[]
+  iniHints.append("Is a "+str(len(str(randNum))))
+  lowLim=randNum-(randNum//10)
+  upLim=randNum+((randNum//10)//10)+20
+  if lowLim<0:
+   lowLim=0
+  if upLim>2000:
+   upLim=2000
+  iniHints.append("Number is between "+str(lowLim)+" - "+str(upLim))
+  if randNum%2==0:
+   evnodd="Number is even"
+  else:
+   evnodd="Number is odd"
+  iniHints.append(evnodd)
+  return iniHints
+
+
 def check(randNum,guessNum,gc):
-  if randNum==guessNum:
-   return 0
-  if gc<3:
-   if (randNum%2)==0:
-    print ("Number is Even")
-   else:
-    print ("Number is Odd")
+  itrHints=[]
+  
   if guessNum>randNum:
-   print ("Your Number is High")
+   itrHints.append("Your Number is High")
   else:
-   print ("Your Number is Low")
+   itrHints.append("Your Number is Low")
   if abs(randNum-guessNum)>50:
-   print ("You are far away (difference is greater than 50)")
+   itrHints.append("You are far away (difference is greater than 50)")
   elif abs(randNum-guessNum)>10:
-   print ("You are close (difference is between 10 to 50")
+   itrHints.append("You are close!!! (difference is between 10-50)")
   else:
-   print ("You are very close (difference is between 1 to 10")
-  return 1 
-   
+   itrHints.append("You are very close!!! (difference is less than 10")
+  return itrHints  
+    
 
 def main():
-  gc=8
-  randNum=random.randint(0,2000)
-  print ("Guess The Number \nInitial Hints\nMaximum number can be 2000")
-  if randNum<1000:
-   print ("Current Number is below 1000")
-  else:
-   print ("Number is above or equal to 1000")
+  while True:
+   c=gc=8
+   itrHints=[]
+   randNum=random.randint(0,2000)
+   for i in range(0,c):
+     os.system("clear")
+     print ("Guess The Number \nInitial Hints")
+     iniHints=initHints(randNum)
+     for hint in iniHints:
+      print (hint)
+     for itrHint in itrHints:
+      print (itrHint)
+     print ("You have ",gc," Guesses ")
+     if gc!=8:
+      print ("Previous Guess is ",prevGuess)
+     prevGuess=guessNum=int(input("Enter Guess: "))
+     
+     if guessNum==randNum:
+      stat=0
+      break
+     gc-=1
+     itrHints=check(randNum,guessNum,gc)   
 
-  for i in range(0,gc):
-    print ("You have ",gc," Guesses ")
-    guessNum=int(input("Enter Guess: "))
-    os.system("clear")
-    stat=check(randNum,guessNum,gc)
-    if stat==0:
-     break
-    gc-=1
-
-  if stat==0:
-   print ("Congrats!!! You Guessed Correct Number: ",randNum)
-  else:
-   print ("OOOPSSS!!! You are out of Guesses, Number was: ",randNum)
-
-
+   if stat==0:
+    print ("Congrats!!! You Guessed Correct Number: ",randNum)
+   else:
+    print ("OOOPSSS!!! You are out of Guesses, Number was: ",randNum)
+   ch=input("Play Again (Y/N)")
+   if ch=='N' or ch == 'n':
+    break
+      
 main()
